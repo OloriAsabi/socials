7145b5ce677756029e382093fa1fbff4e5497fb2
OloriAsabi <developerqueenlatifa@gmail.com>
2022-06-20 15:23:13 +0100

    commit on main

--- src/components/index.js
+++ src/components/index.js
@@ -6,4 +6,4 @@ export { default as UserProfile } from './UserProfile';
 export { default as CreatePin } from './CreatePin';
 export { default as Search } from './Search';
 export { default as Login } from './Login';
-export { default as MasonryLayout } from './MasonryLayout';
+export { default as MasonryLayout } from './MasonryLayout'
